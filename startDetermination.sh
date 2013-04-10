c10af40afbf8a1c37f82c81ae8b7938cb73a69af
Attila Nagy <nagy@physik.hu-berlin.de>
2013-04-10 09:12:38 +0200

    removed one file

--- startDetermination.sh
+++
@@ -1,9 +0,0 @@
-## 500/246=2.0325203252,   175/246=0.711382113821
-
-
-./determineBoundAndPlotPotential  24  48   2000    2.0325203252   0.05   0   0.01  0.6  0.01  output/fullPotential/potential_mt500_cutoff_2000_l6_0.05_lambda_0.txt
-./determineBoundAndPlotPotential  24  48   2000    2.0325203252   0.05   -0.1   0.01  0.6  0.01  output/fullPotential/potential_mt500_cutoff_2000_l6_0.05_lambda_-0.1.txt
-./determineBoundAndPlotPotential  24  48   2000    2.0325203252   0.05   -0.2   0.01  0.6  0.01  output/fullPotential/potential_mt500_cutoff_2000_l6_0.05_lambda_-0.2.txt
-./determineBoundAndPlotPotential  24  48   2000    2.0325203252   0.05   -0.3   0.01  0.6  0.01  output/fullPotential/potential_mt500_cutoff_2000_l6_0.05_lambda_-0.3.txt
-./determineBoundAndPlotPotential  24  48   2000    2.0325203252   0.05   -0.4   0.01  0.6  0.01  output/fullPotential/potential_mt500_cutoff_2000_l6_0.05_lambda_-0.4.txt
-./determineBoundAndPlotPotential  24  48   2000    2.0325203252   0.05   -0.5   0.01  0.6  0.01  output/fullPotential/potential_mt500_cutoff_2000_l6_0.05_lambda_-0.5.txt
